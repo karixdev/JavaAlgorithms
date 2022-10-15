ee4fd30b78ad7c0bc0cb094032054b6727a7bd57
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-15 23:39:29 +0200

    Changed Sorting sort method to be void

--- src/main/java/com/github/karixdev/sorting/Sorting.java
+++ src/main/java/com/github/karixdev/sorting/Sorting.java
@@ -3,5 +3,5 @@ package com.github.karixdev.sorting;
 import java.util.List;
 
 public interface Sorting<T extends Comparable<T>> {
-    List<T> sort(List<T> list);
+    void sort(List<T> list);
 }
--- src/test/java/com/github/karixdev/sorting/BubbleSortTest.java
+++ src/test/java/com/github/karixdev/sorting/BubbleSortTest.java
@@ -11,44 +11,44 @@ public class BubbleSortTest {
     private final BubbleSort<String> stringBubbleSort = new BubbleSort<>();
 
     @Test
-    public void GivenEmptyList_WhenSort_ThenReturnsEmptyList() {
+    public void GivenEmptyList_WhenSort_ThenDoesNothing() {
         // given
         List<Integer> list = List.of();
 
         // when
-        List<Integer> result = integerBubbleSort.sort(list);
+        integerBubbleSort.sort(list);
 
         // then
-        Assertions.assertEquals(list, result);
+        Assertions.assertEquals(List.of(), list);
     }
 
     @Test
-    public void GivenNonEmptyIntegerList_WhenSort_ThenReturnsValidlySortedList() {
+    public void GivenNonEmptyIntegerList_WhenSort_ThenSortsElementCorrectly() {
         // given
         List<Integer> list = List.of(0, -1, 20, 13, 120, 11, 11, 15);
 
         // when
-        List<Integer> result = integerBubbleSort.sort(list);
+        integerBubbleSort.sort(list);
 
         // then
         Assertions.assertEquals(
                 List.of(-1, 0, 11, 11, 13, 15, 20, 13),
-                result
+                list
         );
     }
 
     @Test
-    public void GivenNonEmptyStringList_WhenSort_ThenReturnsValidlySortedList() {
+    public void GivenNonEmptyStringList_WhenSort_ThenSortsElementCorrectly() {
         // given
         List<String> list = List.of("aa", "a", "", "a", "aaaa", "aaa", "aa");
 
         // when
-        List<String> result = stringBubbleSort.sort(list);
+        stringBubbleSort.sort(list);
 
         // then
         Assertions.assertEquals(
                 List.of("", "a", "a", "aa", "aa", "aaa", "aaaa"),
-                result
+                list
         );
     }
 }

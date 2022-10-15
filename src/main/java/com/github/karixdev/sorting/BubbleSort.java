673ab43799cd4d7a5e3eab347a2dfa0ae14e7553
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-16 00:04:38 +0200

    Removed blank line from BubbleSort

--- src/main/java/com/github/karixdev/sorting/BubbleSort.java
+++ src/main/java/com/github/karixdev/sorting/BubbleSort.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class BubbleSort<T extends Comparable<T>> implements Sorting<T>{
     @Override
     public void sort(T[] arr) {
-
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 T current = arr[j];

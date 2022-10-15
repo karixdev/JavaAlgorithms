359f81df0aeaaf881e048853d53af8d1f11ea645
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-15 23:56:16 +0200

    Implemented bubble sort

---
+++ src/main/java/com/github/karixdev/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package com.github.karixdev.sorting;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BubbleSort<T extends Comparable<T>> implements Sorting<T>{
+    @Override
+    public void sort(T[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                T current = arr[j];
+                T next = arr[j + 1];
+                if (current.compareTo(next) > 0) {
+                    arr[j] = next;
+                    arr[j + 1] = current;
+                }
+            }
+        }
+    }
+}

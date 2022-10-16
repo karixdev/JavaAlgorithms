a24ecd3d76aaf8452a64253c3440193a3dc3b5bb
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-16 15:24:38 +0200

    Created tests for selectionSort

---
+++ src/test/java/com/github/karixdev/sorting/SelectionSortTest.java
@@ -0,0 +1,48 @@
+package com.github.karixdev.sorting;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SelectionSortTest {
+    @Test
+    public void GivenEmptyList_WhenSort_ThenDoesNothing() {
+        // given
+        Integer[] arr = {};
+
+        // when
+        SortingAlgorithms.selectionSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(new Integer[] {}, arr);
+    }
+
+    @Test
+    public void GivenNonEmptyIntegerList_WhenSort_ThenSortsElementCorrectly() {
+        // given
+        Integer[] arr = {0, -1, 20, 13, 120, 11, 11, 15};
+
+        // when
+        SortingAlgorithms.selectionSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(
+                new Integer[] {-1, 0, 11, 11, 13, 15, 20, 120},
+                arr
+        );
+    }
+
+    @Test
+    public void GivenNonEmptyStringList_WhenSort_ThenSortsElementCorrectly() {
+        // given
+        String[] arr = {"aa", "a", "", "a", "aaaa", "aaa", "aa"};
+
+        // when
+        SortingAlgorithms.selectionSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(
+                new String[] {"", "a", "a", "aa", "aa", "aaa", "aaaa"},
+                arr
+        );
+    }
+}

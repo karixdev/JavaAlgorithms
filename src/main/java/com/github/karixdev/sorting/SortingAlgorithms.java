9dc736cd4f2cb3e149bce008833dcbb239ae4590
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-16 15:17:36 +0200

    Created bubbleSort's short description

--- src/main/java/com/github/karixdev/sorting/SortingAlgorithms.java
+++ src/main/java/com/github/karixdev/sorting/SortingAlgorithms.java
@@ -1,6 +1,10 @@
 package com.github.karixdev.sorting;
 
 public class SortingAlgorithms {
+    /**
+     * Link: <a href="https://en.wikipedia.org/wiki/Bubble_sort">wikipedia page</a>
+     * <p>Average time complexity: O(n^2)</p>
+     */
     public static <T extends Comparable<T>> void bubbleSort(T[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++)  {

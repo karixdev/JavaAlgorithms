ce9531f7ec665a2e25244482cd7614570c0a3dac
karixdev <30869888+karixdev@users.noreply.github.com>
2022-10-18 23:11:27 +0200

    Created tests for quick sort

---
+++ src/test/java/com/github/karixdev/sorting/QuickSortTest.java
@@ -0,0 +1,52 @@
+package com.github.karixdev.sorting;
+
+import com.github.karixdev.sorting.helper.SortingTestCasesHolder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class QuickSortTest implements ISortingAlgorithmTest {
+    @Test
+    @Override
+    public void GivenEmptyList_WhenSort_ThenDoesNothing() {
+        // given
+        Integer[] arr = {};
+
+        // when
+        SortingAlgorithms.quickSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(new Integer[] {}, arr);
+    }
+
+    @Test
+    @Override
+    public void GivenNonEmptyIntegerList_WhenSort_ThenSortsElementCorrectly() {
+        // given
+        Integer[] arr = SortingTestCasesHolder.integerTestCase.getNonSorted();
+
+        // when
+        SortingAlgorithms.quickSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(
+                SortingTestCasesHolder.integerTestCase.getSorted(),
+                arr
+        );
+    }
+
+    @Test
+    @Override
+    public void GivenNonEmptyStringList_WhenSort_ThenSortsElementCorrectly() {
+        // given
+        String[] arr = SortingTestCasesHolder.stringTestCase.getNonSorted();
+
+        // when
+        SortingAlgorithms.quickSort(arr);
+
+        // then
+        Assertions.assertArrayEquals(
+                SortingTestCasesHolder.stringTestCase.getSorted(),
+                arr
+        );
+    }
+}
